0fe20627ded84c70686c2fa13aff32b15830a660
James Benner <james.benner@gmail.com>
2024-11-07 17:13:34 -0500

    Refactor build script

--- scripts/build.js
+++ scripts/build.js
@@ -9,22 +9,30 @@ const esmPath = path.join('dist', 'esm');
 const commonBuildOptions = { bundle: true, entryPoints, platform: 'node', sourcemap: true };
 const typesTmpPath = path.join('dist', 'types-tmp');
 
+// Dependent on the types being built before this is called.
+async function buildCjs() {
+  await esbuild.build({ ...commonBuildOptions, format: 'cjs', outdir: cjsPath });
+
+  const cjsPackageJson = JSON.stringify({ type: 'commonjs' }, null, 2);
+
+  await fs.writeFile(path.join(cjsPath, 'package.json'), cjsPackageJson);
+
+  await fs.cp(typesTmpPath, cjsPath, { recursive: true });
+}
+
+// Dependent on the types being built before this is called.
+async function buildEsm() {
+  await esbuild.build({ ...commonBuildOptions, format: 'esm', outdir: esmPath, packages: 'external' });
+
+  const esmPackageJson = JSON.stringify({ type: 'module' }, null, 2);
+
+  await fs.writeFile(path.join(esmPath, 'package.json'), esmPackageJson);
+
+  await fs.cp(typesTmpPath, esmPath, { recursive: true });
+}
+
 await $`tsc --declaration --declarationMap --emitDeclarationOnly --outDir ${typesTmpPath}`;
 
-await esbuild.build({ ...commonBuildOptions, format: 'cjs', outdir: cjsPath });
-
-const cjsPackageJson = JSON.stringify({ type: 'commonjs' }, null, 2);
-
-await fs.writeFile(path.join(cjsPath, 'package.json'), cjsPackageJson);
-
-await fs.cp(typesTmpPath, cjsPath, { recursive: true });
-
-await esbuild.build({ ...commonBuildOptions, format: 'esm', outdir: esmPath, packages: 'external' });
-
-const esmPackageJson = JSON.stringify({ type: 'module' }, null, 2);
-
-await fs.writeFile(path.join(esmPath, 'package.json'), esmPackageJson);
-
-await fs.cp(typesTmpPath, esmPath, { recursive: true });
+await Promise.all([buildCjs(), buildEsm()]);
 
 await $`rimraf ${typesTmpPath}`;
